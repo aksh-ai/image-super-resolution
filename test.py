d2329a05be68b7fe5508e0283abd5818369a64a9
aksh-ai <akshayakki6899@gmail.com>
2020-07-04 17:57:04 +0530

    Updated testing script

--- test.py
+++ test.py
@@ -5,8 +5,8 @@ from torchvision import transforms
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
-UPSCALE_FACTOR = 4
-MODEL = 'trained_models/generator_4_100.pth'
+UPSCALE_FACTOR = 2
+MODEL = 'trained_models/Generator_2_64_100.pth'
 IMAGE_PATH = str(input('Enter image path: '))
 OUTPATH = 'results/test/'
 
